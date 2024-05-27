e44aefda721bf90b8688781dc54f50fb6fa43ffe
ColorfulBisuketto <72705023+ColorfulBisuketto@users.noreply.github.com>
2024-05-27 22:49:45 +0200

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 </nav>
               </div>
             </header>
+            <br/>
             <main>{children}</main>
           </div>
           <Analytics />
